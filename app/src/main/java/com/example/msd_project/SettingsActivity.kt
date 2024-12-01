3de444698fc67d36702e3d0b5bda6f597a6c4f92
0Thn1el <C22447532@mytudublin.ie>
2024-12-01 18:34:38 +0000

    commenting on the code

--- app/src/main/java/com/example/msd_project/SettingsActivity.kt
+++ app/src/main/java/com/example/msd_project/SettingsActivity.kt
@@ -18,6 +18,7 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_settings)
+        // Setup window insets
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
